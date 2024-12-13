b1d6d17fbdb35e3e779e4aacc3089eb0dc98b5cf
mrinank1301 <mrinank1318@gmail.com>
2024-12-13 12:55:24 +0530

    changed somethings

--- set-1/FToC.py
+++ set-1/FToC.py
@@ -3,10 +3,10 @@ change = input("F/C?:")
 
 if change == "C" or change == "c":
     celsius = (userInput-32)* (5/9)
-    print(celsius)
+    print(f"celsius is {celsius}")
 elif change == "F" or change == "f":
     fahrenheit = (userInput+32)*(9/5)
-    print(fahrenheit)
+    print(f"fahrenheit is {fahrenheit}")
 else:
     print("wrong input can't change please try again") 
 
