7d9dd7894944581897b826897ab374dba6884657
mrinank1301 <mrinank1318@gmail.com>
2024-12-13 13:00:46 +0530

    changed to letters

--- set-1/randomPassword.py
+++ set-1/randomPassword.py
@@ -2,6 +2,6 @@ import random
 import string
 
 length = int(input("Enter the length of the password: "))
-characters = string.ascii_letters + string.digits + string.punctuation
+characters = string.ascii_letters #+ string.digits + string.punctuation can also be written
 password = "".join(random.choice(characters) for i in range(length))
 print(password)
