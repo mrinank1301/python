580262a4acaac961b5db28dd1fcb26e48993660a
mrinank1301 <mrinank1318@gmail.com>
2024-12-13 13:45:53 +0530

    changed somethings

--- set-1/Factorial.py
+++ set-1/Factorial.py
@@ -4,3 +4,14 @@ for i in range(1,number+1):
     factorial = factorial * i
 
 print(factorial)
+
+# with Recurssive funtion
+
+# def fact(n):
+#     if n==1 or n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+
+# result=fact(5)
+# print(f"result is {result}")
--- set-1/randomPassword.py
+++ set-1/randomPassword.py
@@ -4,4 +4,4 @@ import string
 length = int(input("Enter the length of the password: "))
 characters = string.ascii_letters # + string.digits + string.punctuation can also be written
 password = "".join(random.choice(characters) for i in range(length))
-print(password)
+print(f"password is {password}")
