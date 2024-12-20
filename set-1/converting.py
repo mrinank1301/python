d2777f264cf1bc98d4d3e7caa690fc832590c976
mrinank1301 <mrinank1318@gmail.com>
2024-12-20 14:09:08 +0530

    changed code

--- set-1/converting.py
+++ set-1/converting.py
@@ -1,4 +1,20 @@
-number=int(input("enter number:"))
-print(bin(number))
-print(oct(number))
-print(hex(number))
+def toBase10(num,base):
+    return int(num,base)
+
+def fromBase10(num,base):
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if num==0:
+        return "0"
+    result = ""
+    while num>0:
+        result = digits[num%base] + result
+        num = num//base
+    return result
+
+number = int(input("enter number:"))
+sourceBase= int(input("enter the base of the number:"))
+targetBase = int(input("enter the target base:"))
+
+base10Number = toBase10(number,sourceBase)
+convertedNumber=fromBase10(base10Number,targetBase)
+print(f"{number} in base {sourceBase} is {convertedNumber} in base {targetBase}")
